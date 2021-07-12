09be5450892d14e6321ef1643057c053b9ad373e
Dongdong Zhang <dz1061@nyu.edu>
2021-07-12 13:46:01 -0400

    update rsvp

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/a/macros/nyu.edu/s/AKfycbybZXvQtgVojUX6foLWgnASo5NFHyHyn4k3E1WjrZSkNAO6YrfYsC-iNBeMNU5pApM/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
