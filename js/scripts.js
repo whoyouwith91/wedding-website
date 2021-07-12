0a9807e72a0c85673941bf5ac3b8e4b1a71e81c1
Dongdong Zhang <dz1061@nyu.edu>
2021-07-12 15:06:23 -0400

    change invite code

--- js/scripts.js
+++ js/scripts.js
@@ -214,8 +214,8 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'af22898df99e1b822958187d987cbc86'
+            && MD5($('#invite_code').val()) !== 'd91782ae8b0de41ee2b0f3493df6dcd1') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbzeOD818274JIGaAxAD3QK0ZuIEXSfgVkv9oQmBPnUhQmM0Aw/exec', data)
